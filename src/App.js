a812d588b9323550254c754b4e7f8793450e15fa
shaunjacobsen <shaun.d.jacobsen@gmail.com>
2019-07-05 06:53:41 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { MainMap } from './components/MainMap';
+import { DataContainer } from './components/DataContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './styles/styles.css';
+
+class App extends Component {
+  state = {
+    stations: [],
+    selectedFromStation: undefined,
+    fromStationStats: undefined,
+  };
+
+  componentDidMount() {
+    axios.get(`${process.env.REACT_APP_API_URL}/stations`).then(res => {
+      console.log(res);
+      this.setState(
+        prevState => ({ stations: res.data, selectedFromStation: res.data[0] }),
+        () => {
+          this.getStationStatistics(res.data[0].id);
+        },
+      );
+    });
+  }
+
+  handleChangeFromStation = id => {
+    this.setState(
+      prevState => ({
+        ...prevState,
+        selectedFromStation: this.state.stations.find(
+          station => station.id == id,
+        ),
+      }),
+      () => {
+        this.getStationStatistics(id);
+      },
+    );
+  };
+
+  getStationStatistics = (fromStationId, toStationId = undefined) => {
+    const api = process.env.REACT_APP_API_URL;
+    const query = toStationId
+      ? `${api}/stats/from/${fromStationId}/to/${toStationId}`
+      : `${api}/stats/from/${fromStationId}?r=id,start_station_id,count_trips,duration_mean,duration_median,duration_stddev,count_by_date_hour`;
+    axios.get(query).then(res => {
+      this.setState(prevState => ({
+        ...prevState,
+        fromStationStats: res.data,
+      }));
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="map-container">
+          <MainMap
+            stations={this.state.stations}
+            center={
+              this.state.selectedFromStation && [
+                parseFloat(this.state.selectedFromStation.longitude),
+                parseFloat(this.state.selectedFromStation.latitude),
+              ]
+            }
+          />
+        </div>
+        <DataContainer
+          stations={this.state.stations}
+          selectedFromStation={this.state.selectedFromStation}
+          handleChangeFromStation={this.handleChangeFromStation}
+          fromStationStats={this.state.fromStationStats}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
